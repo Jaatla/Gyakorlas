33836212cc2899e97ffd517a98263b9eb623728b
Your Name <you@example.com>
2024-10-30 19:55:42 +0100

    19:55

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -10,12 +10,20 @@ export class ConfigService {
   menuItems:any=new BehaviorSubject([])
 
   constructor(private http:HttpClient) {
-      this.http.get("assets/lang_hu.json").subscribe(
-        (res:any)=> this.menuItems.next(res['menuItems'])
-      )
+     this.loadConfigJson("hu")
+   }
+
+   private loadConfigJson(lang:string){
+    this.http.get("assets/lang_"+lang+".json").subscribe(
+      (res:any)=> this.menuItems.next(res['menuItems'])
+    )
    }
 
   public getLinks(){
     return this.menuItems
+  }
+
+  public setLang(lang:string){
+    this.loadConfigJson(lang)
   }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -9,10 +9,18 @@ import { ConfigService } from '../config.service';
 export class NavComponent {
 
   links:any
+  dropClose=true
+  lang="Magyar"
 
   constructor(private config:ConfigService){
     this.config.getLinks().subscribe(
       (res:any)=>this.links=res
     )
+  }
+
+  setLang(lang:string){
+    this.lang=lang=="hu"?"Magyar":"English"
+    this.config.setLang(lang)
+    this.dropClose=true
   }
 }
